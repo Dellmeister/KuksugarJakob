4b641c3099efc6a0cfe3a1c70ab8b893621a503c
BarreBN <118360174+BarreBN@users.noreply.github.com>
2024-05-18 17:24:03 +0200

    updated styles.css location call

--- Application/app.py
+++ Application/app.py
@@ -9,17 +9,11 @@ client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Function to load CSS
 def load_css(file_name):
-    # Get the directory of the current script
-    script_dir = os.path.dirname(__file__)
-
-    # Construct the absolute path to the CSS file
-    file_path = os.path.join(script_dir, file_name)
-
-    with open(file_path) as f:
+    with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # Load the CSS
-load_css('/mnt/data/styles.css')
+load_css('styles.css')
 
 def get_recommendations(text, experience, language, employment_type, location, driving_license, education):
     if language == 'Swedish':
